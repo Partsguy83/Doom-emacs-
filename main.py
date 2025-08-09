125061b2dcc84c1b2779e2737c4ac3f7e36faddc
zach <partsguy83@gmail.com>
2025-08-09 16:54:16 -0500

    Changes to be committed:

--- main.py
+++ main.py
@@ -2,6 +2,76 @@
 
 # Test Program for python environment
 print('This is a test')
-
 # Error Test
-print (this is an error
+print (this is a error
+# understanding variables and data types
+# creating variables is easy , write variable name then = and then the value
+name = zach
+age = 41
+height = 5.4
+# above delcares three variables , name which is a string , age is an int and height is a float
+# the = is called an "assignment operator"
+# basically stands for assign the value to the right of the variable
+# variables must start with a letter or underscore
+# they can contain letters , numbers and underscores
+# are case sensitive
+# examples of good variables myVariable , _temp , AGE
+# not good variables 1var , my-variable , For
+# Data types
+# Integers - whole numbers
+# Floats - decimal numbers
+# Strings - text enclosed in quotes
+# Booleans - True or False values used for conditonal statements
+# Integer
+integer_example = 5
+# float
+float_example = 3.14
+# string
+string_example = "Good Morning!"
+# boolean
+boolean_example = True
+# different types of variables can be converted between different types
+age = 41  #integer
+age_as_string = str(age) #it's a string now
+
+# Examples:
+# create a variable called my_name and assign your name to it
+my_name = Zach
+# create another variable called my_age
+my_age = 41
+# multiply my_age by 2 and store the result in a new variable called double_age
+double_age = my_age *2
+
+# Basic operators
+# Addition
+result = 3 + 4  #result is 7
+# Subtraction
+result = 4 - 3  #result is 1
+# Multiplication
+result = 4 * 3  #result is 12
+# Division
+result = 10 / 2  #result is 5
+# Floor division
+result = 9 // 2  #result is 4
+# Modulus
+result = 9 % 2   #result is 1
+# Exponentiation
+result = 3 ** 2  #result is 9
+# arithmetic operators work with variables too
+a = 5
+b = 2
+addition = a + b  #result is 7
+
+# comparison operators help us make decisions in our code by comparing values
+# (==)  Equal to
+# !=    Not equal to
+# >     Greater than
+# <     Less than
+# >=    greater than or equal to
+# <=    less than or equal to
+
+# logical operators are used to combine condtional statements
+# and  And
+# or   or
+# not  not
+# order of operations is important always us parentthese in your code
